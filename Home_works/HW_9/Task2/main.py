5ce804a28b89cfa8442eeb70d5ddf70bc5311061
Vladimir <vladimirtikhonov230@gmail.com>
2022-12-13 03:52:44 +0300

    Home work 9 added

---
+++ Home_works/HW_9/Task2/main.py
@@ -0,0 +1,148 @@
+import random
+import requests
+import time
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
+
+import strings as st
+
+def getToken():
+    token = ''
+    with open(st.PATH, 'r', encoding='utf-8') as file:
+        token = file.read()
+    
+    return token
+
+def isWin(arr, who):
+    if (((arr[0] == who) and (arr[4] == who) and (arr[8] == who)) or
+            ((arr[2] == who) and (arr[4] == who) and (arr[6] == who)) or
+            ((arr[0] == who) and (arr[1] == who) and (arr[2] == who)) or
+            ((arr[3] == who) and (arr[4] == who) and (arr[5] == who)) or
+            ((arr[6] == who) and (arr[7] == who) and (arr[8] == who)) or
+            ((arr[0] == who) and (arr[3] == who) and (arr[6] == who)) or
+            ((arr[1] == who) and (arr[4] == who) and (arr[7] == who)) or
+            ((arr[2] == who) and (arr[5] == who) and (arr[8] == who))):
+        return True
+    return False
+
+def countUndefinedCells(cellArray):
+    counter = 0
+    for i in cellArray:
+        if i == st.SYMBOL_UNDEF:
+            counter += 1
+    return counter
+
+def game(callBackData):
+    # -------------------------------------------------- global message  # использование глобальной переменной message
+    message = st.ANSW_YOUR_TURN  # сообщение, которое вернется
+    alert = None
+
+    buttonNumber = int(callBackData[0])  # считывание нажатой кнопки, преобразуя ее из строки в число
+    if not buttonNumber == 9:  # цифра 9 передается в первый раз в качестве заглушки. Т.е. если передана цифра 9, то клавиатура для сообщения создается впервые
+        charList = list(callBackData)  # строчка callBackData разбивается на посимвольный список "123" -> ['1', '2', '3']
+        charList.pop(0)  # удаление из списка первого элемента: который отвечает за выбор кнопки
+        if charList[buttonNumber] == st.SYMBOL_UNDEF:  # проверка: если в нажатой кнопке не выбран крестик/нолик, то можно туда сходить крестику
+            charList[buttonNumber] = st.SYMBOL_X  # эмуляция хода крестика
+            if isWin(charList, st.SYMBOL_X):  # проверка: выиграл ли крестик после своего хода
+                message = st.ANSW_YOU_WIN
+            else:  # если крестик не выиграл, то может сходит бот, т.е. нолик
+                if countUndefinedCells(charList) != 0:  # проверка: есть ли свободные ячейки для хода
+                    # если есть, то ходит бот (нолик)
+                    isCycleContinue = True
+                    # запуск бесконечного цикла т.к. необходимо, чтобы бот походил в свободную клетку, а клетка выбирается случайным образом
+                    while (isCycleContinue):
+                        rand = random.randint(0, 8)  # генерация случайного числа - клетки, в которую сходит бот
+                        if charList[rand] == st.SYMBOL_UNDEF:  # если клетка неопределенна, то ходит бот
+                            charList[rand] = st.SYMBOL_O
+                            isCycleContinue = False  # смена значения переменной для остановки цикла
+                            if isWin(charList, st.SYMBOL_O):  # проверка: выиграл ли бот после своего кода
+                                message = st.ANSW_BOT_WIN
+
+        # если клетка, в которую хотел походить пользователь уже занята:
+        else:
+            alert = st.ALERT_CANNOT_MOVE_TO_THIS_CELL
+
+        # проверка: остались ли свободные ячейки для хода и что изначальное сообщение не поменялось (означает, что победителя нет, и что это был не ошибочный ход)
+        if countUndefinedCells(charList) == 0 and message == st.ANSW_YOUR_TURN:
+            message = st.ANSW_DRAW
+
+        # формирование новой строчки callBackData на основе сделанного хода
+        callBackData = ''
+        for c in charList:
+            callBackData += c
+
+    # проверка, что игра закончилась (message равно одному из трех вариантов: победил Х, 0 или ничья):
+    if message == st.ANSW_YOU_WIN or message == st.ANSW_BOT_WIN or message == st.ANSW_DRAW:
+        message += '\n'
+        for i in range(0, 3):
+            message += '\n | '
+            for j in range(0, 3):
+                message += callBackData[j + i * 3] + ' | '
+        callBackData = None  # обнуление callBackData
+    return message, callBackData, alert
+
+def getKeyboard(callBackData):
+    keyboard = [[], [], []] 
+
+    if callBackData != None: 
+        for i in range(0, 3):
+            for j in range(0, 3):
+                keyboard[i].append(InlineKeyboardButton(callBackData[j + i * 3], callback_data=str(j + i * 3) + callBackData))
+
+    return keyboard
+
+
+def newGame(update, _):
+    data = ''
+    for i in range(0, 9):
+        data += st.SYMBOL_UNDEF
+
+    update.message.reply_text(st.ANSW_YOUR_TURN, reply_markup=InlineKeyboardMarkup(getKeyboard(data)))
+
+
+def button(update, _):
+    query = update.callback_query
+    callbackData = query.data 
+
+    message, callbackData, alert = game(callbackData) 
+    if alert is None:  # если не получен сигнал тревоги (alert==None), то редактируем сообщение и меняем клавиатуру
+        query.answer()  # обязательно нужно что-то отправить в ответ, иначе могут возникнуть проблемы с ботом
+        query.edit_message_text(text=message, reply_markup=InlineKeyboardMarkup(getKeyboard(callbackData)))
+    else:  # если получен сигнал тревоги (alert!=None), то отобразить сообщение о тревоге
+        query.answer(text=alert, show_alert=True)
+
+
+def start_command(update, _):
+    update.message.reply_text(st.ANSW_START)
+
+def help_command(update, _):
+    update.message.reply_text(st.ANSW_HELP)
+
+def exception_command(update, _):
+    update.message.reply_text(st.ANSW_EXCEPTION)
+
+def image_command(update, _):
+    photo_file = update.message.photo[-1].get_file()
+    upload = requests.post(st.API_URL_UPLOAD, data=photo_file, verify=False)
+    time.sleep(5)
+    download = requests.post(st.API_URL_DOWNLOAD, verify=False)
+    update.message.reply_photo(photo=download)
+    # photo_file.download('user_photo.jpg')
+    # update.message.reply_text("Image received")
+
+if __name__ == '__main__':
+    updater = Updater(getToken())  # получения токена из файла 'token.txt' и инициализация updater
+
+    # добавление обработчиков
+    updater.dispatcher.add_handler(CommandHandler('start', start_command))
+    updater.dispatcher.add_handler(CommandHandler('play', newGame))
+    updater.dispatcher.add_handler(CommandHandler('help', help_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.photo, image_command))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, exception_command))
+    updater.dispatcher.add_handler(CallbackQueryHandler(button)) 
+    
+
+    # Запуск бота
+    updater.start_polling()
+    updater.idle()
---
+++ Home_works/HW_9/Task2/strings.py
@@ -0,0 +1,20 @@
+PATH = "E:\GeekBrains\Python_Start\Home_works\HW_9\Task2\Token.txt"
+API_URL_UPLOAD = "https://mass-images.pro/ru/api/upload"
+API_URL_DOWNLOAD = "https://mass-images.pro/download"
+
+# символы, которые используются
+SYMBOL_X = '❌'
+SYMBOL_O = '⭕'
+SYMBOL_UNDEF = '◻'
+
+# ответы бота
+ANSW_YOUR_TURN = 'Ваш ход'
+ANSW_YOU_WIN = 'Вы победили'
+ANSW_BOT_WIN = 'Победил бот'
+ANSW_DRAW = 'Ничья'
+ANSW_START = "Привет, я бот для игры в крестики-нолики.\nПредлагаю ознакомиться с моим функционалом /help"
+ANSW_HELP = "/play - Начать игру."
+
+# ошибки
+ALERT_CANNOT_MOVE_TO_THIS_CELL = 'Нажимать можно только на ' + SYMBOL_UNDEF
+ANSW_EXCEPTION = "Я такого не знаю :(\nПредлагаю ознакомиться с моим функционалом /help"
